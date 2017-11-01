a12e02e05891c63766c57e82ceabda5d85e1624c
Amber <amhiggin@tcd.ie>
2017-11-01 15:37:56 +0000

    Adding scripts and .class files for terminal execution

--- Scripts/compile.sh
+++ Scripts/compile.sh
@@ -1,1 +1,16 @@
 #!/bin/sh
+#set -x
+
+###########################
+## Shell script to compile the 
+## https://www.github.com/amhiggin/ChatroomServer java project.
+## Assumed Java and Git installed on host machine.
+###########################
+
+
+echo "Running compilation on Chatroom Server project"
+
+# Compile with dependencies
+javac -cp lib\* src\main\java\*.java
+
+echo "Done."
--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,1 +1,18 @@
 #!/bin/sh
+#set -x
+
+###########################
+## Shell script to run the compiled chat server from 
+## https://www.github.com/amhiggin/ChatroomServer github repository.
+## Requires running compile.sh first, and then running script with argument $1 portNumber
+###########################
+
+
+JAVAHOME=/usr/local/java/jdk1.8.0_101/
+
+portNumber = $1
+
+
+$JAVAHOME/bin/java -showversion -cp  $CLASSPATH\*  main.java.ChatroomServer ${port_number}
+
+echo "Starting up chatroom server on port ${portNumber}"
