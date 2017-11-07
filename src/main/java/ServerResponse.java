80cd1f67b17213de4f6aeba5d3e914da53898419
Amber <amhiggin@ITSERV.SCSS.TCD.IE>
2017-11-07 18:15:59 +0000

    Adding newline characters to try to correct parsing

--- src/main/java/ServerResponse.java
+++ src/main/java/ServerResponse.java
@@ -5,12 +5,12 @@ public enum ServerResponse {
 	 * To be used with String.format(msg, args[])
 	 */
 
-	JOIN("JOINED_CHATROOM: %s\n" + "SERVER_IP: %s\n" + "PORT: %s\n" + "ROOM_REF: %s\n" + "JOIN_ID: %s"), LEAVE(
-			"LEFT_CHATROOM: %s\n" + "JOIN_ID: %s"), CHAT(
+	JOIN("JOINED_CHATROOM: %s\n" + "SERVER_IP: %s\n" + "PORT: %s\n" + "ROOM_REF: %s\n" + "JOIN_ID: %s\n"), LEAVE(
+			"LEFT_CHATROOM: %s\n" + "JOIN_ID: %s\n"), CHAT(
 					"CHAT: %s\n" + "JOIN_ID: %s\n" + "CLIENT_NAME: %s\n" + "MESSAGE: %s\n\n"), DISCONNECT(
-							"DISCONNECT: %s\n" + "PORT: %s\n" + "CLIENT_NAME: %s"), HELO(
-									"HELO %s\n" + "IP: %s\n" + "Port: %s\n" + "StudentID: %s"), ERROR(
-											"ERROR_CODE: %s\n" + "ERROR_DESCRIPTION: %s");
+							"DISCONNECT: %s\n" + "PORT: %s\n" + "CLIENT_NAME: %s\n"), HELO(
+									"HELO %s\n" + "IP: %s\n" + "Port: %s\n" + "StudentID: %s\n"), ERROR(
+											"ERROR_CODE: %s\n" + "ERROR_DESCRIPTION: %s\n");
 
 	private String value;
 
